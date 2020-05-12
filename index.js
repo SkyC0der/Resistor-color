6079f68f2a082b3010394244ab67468123069174
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-12 19:29:07 +0100

    Created Resistor Code Function

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const decodedValue = (array) => {
-   //type code here
+const colors = ["black", "brown", "red", "orange","yellow", "green", "blue", "violet", "grey","white"]
+
+const decodedValue = (x) => {
+   let arr = x.slice(0,2)
+   let result = arr.map((i) => colors.indexOf(i)).join('') ; 
+   return parseInt(result,10)
   };
 
   module.exports = decodedValue
